1b0630bafd4a1aa82c2b6ce35f9aa35863d12430
devlee77 <tjdcks0928@naver.com>
2021-02-10 11:02:20 +0900

    BOJ 2747, 2748 complete

---
+++ 2747번 피보나치 수.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+long long arr[46];
+
+long long dp(int n) {
+	if (n == 0) return 0;
+	if (arr[n] != 0)
+		return arr[n];
+	return arr[n] = dp(n - 1) + dp(n - 2);
+}
+
+int main(void) {
+	int n;
+	arr[0] = 0;
+	arr[1] = 1;
+
+	cin >> n;
+	cout << dp(n);
+	return 0;
+}
