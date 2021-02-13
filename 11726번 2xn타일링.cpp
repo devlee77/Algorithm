ac08f3a415d70eba0180cb28a0ef9e8e25fea8e3
devlee77 <tjdcks0928@naver.com>
2021-02-13 18:05:32 +0900

    BOJ 11726,11727 complete

---
+++ 11726번 2xn타일링.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+long arr[1001];
+
+long dp(int n) {
+	if (arr[n])
+		return arr[n];
+	return arr[n] = (dp(n - 1) + dp(n - 2)) % 10007;
+}
+
+int main(void) {
+	int n;
+	cin >> n;
+	arr[1] = 1;
+	arr[2] = 2;
+	cout << dp(n);
+	return 0;
+}
